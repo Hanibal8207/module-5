f9311c9e348599f7ab55949c65fb595e866e05f9
unknown <tesfamichaelhanibal7@gmail.com>
2022-03-14 17:12:11 -0800

    this is a new restaurant menu for an indian restaurant in my area

---
+++ css/CREATE TABLE customers(id INTEGER PRIMAR.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customers(id INTEGER PRIMARY KEY,Customer_name TEXT,Email_Adress TEXT, age INTEGER);
+INSERT INTO customers VALUES(1, Hannibal, tesfamichaelhanibal7@gmail.com,26);
+INSERT INTO customers VALUES(2, Genet, tesfamichaelhanibal7@gmail.com,26);
+INSERT INTO customers VALUES(3, Saron, tesfamichaelhanibal7@gmail.com,24);
+INSERT INTO customers VALUES(4, Hidri, tesfamichaelhanibal7@gmail.com,28);
+INSERT INTO customers VALUES(5, Rigat, tesfamichaelhanibal7@gmail.com,33);
+INSERT INTO customers VALUES(6, Tewelde, tesfamichaelhanibal7@gmail.com,66);
+INSERT INTO customers VALUES(7, Sinit, tesfamichaelhanibal7@gmail.com,26);
+SELECT * FROM customers;
